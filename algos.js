580bcd35d4e9b52b57668006a6618947a32d5f05
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-12-02 09:33:05 -0600

    dec 2 algos completed

--- algos.js
+++ algos.js
@@ -52,3 +52,43 @@ const merge = function(nums1, m, nums2, n) {
     k--;
   }
 };
+
+
+// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+// Return the running sum of nums
+
+const runningSum = function(nums) { 
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    nums[i] = sum;
+  }
+  return nums;
+}
+
+
+
+// Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+// A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+const buildArray = function(nums) {
+  let ans = [];
+  for (let i = 0; i < nums.length; i++) {
+    ans.push(i + 1);
+    ans.push(nums[i]);
+  }
+  return ans;
+}
+
+// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+const shuffle = function(nums, n) {
+  let ans = [];
+  for (let i = 0; i < n; i++) {
+    ans.push(nums[i]);
+    ans.push(nums[i + n]);
+  }
+  return ans;
+}
